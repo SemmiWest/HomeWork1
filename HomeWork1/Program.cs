d9562fac403ed8ceecb05e67f8c321a046a7c88a
SemmiWest <chubaka.tm@mail.ru>
2023-03-10 14:30:28 +0700

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя: ");
+string name = Console.ReadLine();
+    Console.WriteLine($"Привет {name}");
