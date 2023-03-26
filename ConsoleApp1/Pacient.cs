5359288d624a29cbae921b0f91bd35de3b82f206
SemmiWest <chubaka.tm@mail.ru>
2023-03-26 16:26:31 +0700

    HomeWork3

---
+++ ConsoleApp1/Pacient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pacient
+    {
+        public string Name { get; set; }
+        public string Pol { get; set; }
+        public string Age { get; set; }
+
+        public int Status(int value)  {
+            /*Random rnd = new Random();
+            int value = rnd.Next(0, 1);*/
+            if (value == 1) { Console.WriteLine("Пациент : Жалоба"); }
+            else { Console.WriteLine("Пациент : Жалоб нет"); }
+            return value;
+            
+
+        }
+
+        public void BuyMedicaments() {
+            Console.WriteLine("Пациент : Поход в аптеку");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+using ConsoleApp1;
+
+Doctor Therapyst = new Doctor()
+{
+    Name = "Василий",
+    Post = "Заведующий отделением",
+    Specialization = "Терапевт"
+};
+
+Doctor Surgeon = new Doctor()
+{
+    Name = "Михаил",
+    Post = "Дежурный врач",
+    Specialization = "Хирург"
+};
+
+Pacient pacient = new Pacient()
+{
+    Name = "Максим",
+    Pol = "Мужской",
+    Age = "25"
+};
+
+Console.WriteLine("Обращение : ");
+Console.WriteLine("1.Иммунитет");
+Console.WriteLine("2.Часть тела");
+ var Inicialization  = Console.ReadLine();
+int ToConvert = Convert.ToInt32(Inicialization);
+
+if (ToConvert == 1  ) 
+{
+    Random rnd = new Random();
+    int value = rnd.Next(0, 1);
+    if (value == 1)
+    {
+        var Cons = pacient.Status;
+        Therapyst.Analiz(value);
+        var buyMedicaments = pacient.BuyMedicaments;
+        var godBuy = Therapyst.GoodBuy;
+    }
+    else { Therapyst.Analiz(value);
+        var godBuy = Therapyst.GoodBuy;
+    }
+     
+       
+}
+else {
+    Random rnd = new Random();
+    int value = rnd.Next(0, 1);
+    if (value == 1)
+    {
+        var Cons = pacient.Status;
+        Surgeon.Analiz(value);
+        var buyMedicaments = pacient.BuyMedicaments;
+        var godBuy = Therapyst.GoodBuy;
+    }
+    else if (value == 0)
+    {
+        Therapyst.Analiz(value);
+        var godBuy = Therapyst.GoodBuy;
+    }
+}
+
+
