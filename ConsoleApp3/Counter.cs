75ca7e34f41ea50725f075f2f1b9dc7afb0666f5
SemmiWest <chubaka.tm@mail.ru>
2023-04-02 03:13:57 +0700

    HomeWork4

---
+++ ConsoleApp3/Counter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Counter
+    {
+        
+        public event EventHandler Inicialization;
+        
+        
+        public  void Inicailizat()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 77)
+                {
+                    Console.WriteLine(i);
+                    Inicialization(this, new EventArgs());
+                    break;
+                }
+            }
+        }
+
+    } }
+
+       
+    
+  
+
+  
+               
+            
+            
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp3
+{
+    internal class Program
+        
+    {
+        public static void OnInicialization(object sender, EventArgs e)
+        {
+            Handler1.Write();
+            Handler2.Write();
+        }
+        public delegate void EvetHandler();
+        
+        static void Main(string[] args)
+        {
+            
+            Counter counter = new Counter();
+            counter.Inicialization += OnInicialization;
+            counter.Inicailizat();
+
+        }
+
+    }
+}
