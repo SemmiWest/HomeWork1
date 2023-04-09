2028880eb2004200a96d8ab8783ef60d3ad0c7ef
SemmiWest <chubaka.tm@mail.ru>
2023-04-10 01:20:55 +0900

    HomeWork5(PartOne)

---
+++ ConsoleApp5/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Info
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }    
+
+        public string DateOfRecv { get; set; }
+    }
+}
